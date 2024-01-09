b23a3920f868b84472c4d0cd526c4365311f0f66
Sofie <sohe0002@stud.kea.dk>
2024-01-09 13:22:44 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var burgerMenu = document.getElementById('burger-menu');
+    var menu = document.getElementById('menu');
+
+    burgerMenu.addEventListener('click', function () {
+      menu.classList.toggle('active');
+    });
+  });
+
